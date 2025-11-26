1122420888d48c737fb476eaf1709741f5f56ec3
Harish Singaravelan <hs7569@rit.edu>
2025-11-26 15:13:35 -0500

    Repaired Tests

--- Tests/test_dataloader.py
+++ Tests/test_dataloader.py
@@ -1,5 +1,8 @@
 # tests/test_dataloader.py
 
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import torch
 from torch.utils.data import Dataset
 
--- Tests/test_model.py
+++ Tests/test_model.py
@@ -1,5 +1,8 @@
 import torch
 
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from modeling.model import get_model
 
 
